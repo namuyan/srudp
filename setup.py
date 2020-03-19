ac6852b54be892d7d34f69526394e0a8b6ebfa35
namuyan <thhjuu@yahoo.co.jp>
2020-03-19 19:47:47 +0900

    change: don't support 3.5

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
     license="MIT Licence",
     keywords='RUDP UDP P2P hole-punching',
     classifiers=[
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
